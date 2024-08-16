b690bb0eaae4c42419bc276f27a7d415f3b97338
muhawais@gmail.com <muhawais@gmail.com.in>
2024-08-16 07:01:22 +0000

    story

---
+++ awais.py
@@ -0,0 +1,22 @@
+print("pakistan")
+num1=5
+num2=5
+sum=num1+num2
+print(sum) 
+num1=5
+num2=10
+diff=num2-num1
+print(diff)
+num1=10
+num2=5
+product=num1*num2
+print(product)
+num1=10
+num2=5
+division=num1// num2
+print(division)
+age=20
+print("This is my age", 20)
+print("My name is Muhammad Awais s/o Saeed Iqbal")
+print("I belong to Chichawatni,District Sahiwal")
+print("My education second year(IcS)")
