05fe76c747a8bb8590c8574ad266566465bc5b86
muhawais@gmail.com <muhawais@gmail.com.in>
2024-08-20 09:25:26 +0000

    ADD LINES

--- awais.py
+++ awais.py
@@ -11,5 +11,15 @@ height = float(input("Enter the height of the cylinder: "))
 # Volume Calculation
 volume = math.pi * (radius ** 2) * height
 print(f"The Volume of the cylinder is: {volume: .2f} cubic units")
-
-
+current_year = 2024
+birth_year = 2004
+age = current_year - birth_year
+print("This is my age",age)
+lenght = 10
+width = 5
+print("The area of the rectangle is:", lenght * width)
+print(3.14 * 7 ** 2)
+print((98 - 32) * 5 / 9)
+print((37 * 9 / 5) + 32)
+print(divmod(125, 60))
+print((400 / 500) * 100)
